a15b0a1cd11c2d09cc0b478689783c86cf3c83f4
devenmundada <mundadadeven@gmail.com>
2026-05-18 01:56:33 +0530

    Time: 3 ms (62.18%) | Memory: 8.2 MB (17.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -11,8 +11,29 @@ public:
         return ans;
     }
 
-    int fib(int n) {
-        int ans = solveusingrecursion(n);
-        return ans;
+    // using DP solution
+    // top-down approch
+    // type of dp -> 1D dp -> dp array - 1D
+       int solveusingmem(int n, vector<int> &dp ){
+        // base case
+        if(n == 0 || n == 1){
+            return n;
+        }
+    // step 3 : base case ke baad, check if ans already exists or not 
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        // step 2: Store and retuen the dp array
+        dp[n] = solveusingmem(n-1,dp) + solveusingmem(n-2,dp);
+        return dp[n];
     }
+
+    int fib(int n) {
+        // int ans = solveusingrecursion(n);
+        // return ans;
+    // step - 1 :
+        vector<int> dp(n+1,-1);
+         return solveusingmem(n,dp);
+    } 
 };
