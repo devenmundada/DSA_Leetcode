ecd92f60b48973d77fe9fc5191272ed708aeb8f5
devenmundada <mundadadeven@gmail.com>
2026-05-18 16:03:14 +0530

    Time: 580 ms (8.99%) | Memory: 296.7 MB (5.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -38,6 +38,28 @@ public:
         return dp[curr][prev+1];
     }
 
+    // Using Tabular form or bottom up approch
+    int solveuisngTab(vector<int>& arr){
+        int n = arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+        // Recurssion range
+        // curr -> 0 to n
+        // prev -> -1 to curr
+        // reverse it and apply loop
+        for(int curr = n-1;curr >= 0;curr--){
+            for(int prev = curr-1;prev >= -1;prev--){
+                // now pasting the logic and converting into DP
+            int inc = 0;
+            if(prev == -1 || arr[curr] > arr[prev]){
+                inc = 1 + dp[curr+1][curr+1];
+            }
+            int exc = 0 + dp[curr+1][prev+1];
+            dp[curr][prev+1] = max(inc,exc);
+              }
+            }
+            return dp[0][0];
+        }
 
     int lengthOfLIS(vector<int>& nums) {
         int curr = 0;
