6c3b8bdbda1ebbb23a380a430c7d5b42dcdd2f4d
devenmundada <mundadadeven@gmail.com>
2026-09-21 18:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (10.28%) - LeetSync

--- LeetCode/724-find-pivot-index/find-pivot-index.cpp
+++ LeetCode/724-find-pivot-index/find-pivot-index.cpp
@@ -20,7 +20,28 @@ public:
 
     }
 
+    int prefixSumApproch(vector<int> &nums){
+        vector<int>lsum(nums.size(),0);
+        vector<int>rsum(nums.size(),0);
+
+        // calculate lsum array
+        for(int i = 1;i < nums.size();i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
+        }
+
+        for(int i = nums.size()-2;i >= 0;i--){
+            rsum[i] = rsum[i+1] + nums[i+1];
+        }
+        // now check if the lsum and the rsum matches at any point
+        for(int i = 0;i < nums.size();i++){
+            if(lsum[i] == rsum[i])
+            return i;
+        }
+        return -1;
+    }
+
     int pivotIndex(vector<int>& nums) {
-        return bruteforce(nums);
+        // return bruteforce(nums);
+        return prefixSumApproch(nums);
     }
 };
